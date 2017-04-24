818d3e577b5388f0b1ab16d4a512bdebaeb3c483
Luis David Ocampo Aguirre <Luisda199824@gmail.com>
2017-04-24 09:57:43 -0500

    Organización final modelos

--- modelsAdmin/models.py
+++ modelsAdmin/models.py
@@ -21,5 +21,89 @@ class Usuario(models.Model):
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
 
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
     def __unicode__(self):
-        pass
+        return self.nombre + " " + self.apellido
+
+class Administrador(models.Model):
+    usuario = models.ForeignKey(Usuario)
+    estadoCuenta = models.BooleanField(default=True)
+    municipio = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Administrador'
+        verbose_name_plural = 'Administradores'
+
+    def __unicode__(self):
+        return str(self.usuario)
+
+class Suscriptor(models.Model):
+    administrador = models.ForeignKey(Administrador)
+    usuario = models.ForeignKey(Usuario)
+    estadoCuenta = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Suscriptor'
+        verbose_name_plural = 'Suscriptores'
+
+    def __unicode__(self):
+        return str(self.usuario)
+
+class ListaSolicitudesAdministrador(models.Model):
+    administrador = models.ForeignKey(Administrador)
+    evaluada = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Lista de solicitudes del Administrador'
+        verbose_name_plural = 'Lista de solicitudes del Administrador'
+
+    def __unicode__(self):
+        return "Evaluación: " + str(self.administrador.usuario)
+        return str(self.usuario)
+
+class ListaSolicitudesSuscriptor(models.Model):
+    suscriptor = models.ForeignKey(Suscriptor)
+    evaluada = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Lista de solicitudes del Suscriptor'
+        verbose_name_plural = 'Lista de solicitudes del Suscriptores'
+
+    def __unicode__(self):
+        return "Evaluación: " + str(self.suscriptor.usuario)
+
+class Interes(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Interes'
+        verbose_name_plural = 'Intereses'
+
+    def __unicode__(self):
+        return self.nombre
+
+class ListaInteresAdministrador(models.Model):
+    administrador = models.ForeignKey(Administrador)
+    interes = models.ForeignKey(Interes)
+
+    class Meta:
+        verbose_name = 'Lista de intereses de Administradores'
+        verbose_name_plural = 'Lista de intereses de Administradores'
+
+    def __unicode__(self):
+        return "Interes: %s Administrador: %s" % (self.interes.nombre, str(self.administrador.usuario))
+        return self.nombre
+
+class ListaInteresSuscriptor(models.Model):
+    suscriptor = models.ForeignKey(Suscriptor)
+    interes = models.ForeignKey(Interes)
+
+    class Meta:
+        verbose_name = 'Lista de intereses de Suscriptores'
+        verbose_name_plural = 'Lista de intereses de Suscriptores'
+
+    def __unicode__(self):
+        return "Interes: %s Suscriptor: %s" % (self.interes.nombre, str(self.suscriptor.usuario))
