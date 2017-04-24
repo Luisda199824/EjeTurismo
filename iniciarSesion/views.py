ee33ed770c9b6dc27a400a64b8d2e6ae2587afe7
Luis David Ocampo Aguirre <Luisda199824@gmail.com>
2017-04-24 09:38:55 -0500

    Organización de Código Iniciar sesion

--- iniciarSesion/views.py
+++ iniciarSesion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def iniciarSesion(request):
+    template = loader.get_template('iniciarSesion/index.html')
+    ctx = {}
+    return HttpResponse(template.render(ctx, request))
