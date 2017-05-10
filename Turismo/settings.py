331e32d0a78e22420f655b118fc9067e5c69f9b5
Luis David Ocampo Aguirre <Luisda199824@gmail.com>
2017-05-10 00:54:34 -0500

    Organización de URL Media para imagenes subidas

--- Turismo/settings.py
+++ Turismo/settings.py
@@ -110,13 +110,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#  os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media Files (Jpg, png)
-MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
